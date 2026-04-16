cf4bdbd9e13140f2cbf429c2d16641f941482915
mamathaudogere-hub <mamathaudogere@gmail.com>
2026-04-16 06:57:13 +0530

    125.java

---
+++ Two_Sum/125.java
@@ -0,0 +1,16 @@
+//............... Valid Palindrome .................(125.java)
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0,right=s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++; // for any special characters skip that
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))) return false;
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+}
