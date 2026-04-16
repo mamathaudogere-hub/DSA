83df8dc9aa8a4f0d4e3dcce4a750fdc91e039202
mamathaudogere-hub <mamathaudogere@gmail.com>
2026-04-16 06:47:20 +0530

    167.java

--- Two_Sum/167.java
+++ Two_Sum/167.java
@@ -1,1 +1,16 @@
+//.............. Two Sum II - Input Array Is Sorted ...............(167-leetcode)
 
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int left=0,right=n-1;
+        while(left<=right){
+           int sum=0;
+           sum=numbers[left]+numbers[right];
+           if(sum==target) return new int[]{left+1,right+1};
+           if(sum>target) right--;
+           else left++;
+        }
+        return new int[]{-1,-1};
+    }
+}
