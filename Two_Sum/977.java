bc1f985533724d821d50911d0ac15d643472179f
mamathaudogere-hub <mamathaudogere@gmail.com>
2026-04-24 10:04:50 +0530

    977.java

---
+++ Two_Sum/977.java
@@ -0,0 +1,12 @@
+//...................... Squares of a Sorted Array .................(977-leetcode)
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
